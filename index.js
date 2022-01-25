125a3bf6a9b27d8be697a9f359704f692eb0d368
Samuel Braun <42311986+MindLaborDev@users.noreply.github.com>
2022-01-25 21:16:21 +0100

    Added titles

--- index.js
+++ index.js
@@ -84,14 +84,14 @@ document.addEventListener('DOMContentLoaded', async () => {
                 }
             </style>
             <div class="ssbtns-wrapper">
-                <div class="twitch"><i class="fab fa-twitch"></i></div>
-                <div class="quick"><i class="fas fa-cloud-download-alt"></i></div>
-                <div class="beatsaver">
+                <div class="twitch" title="!bsr"><i class="fab fa-twitch"></i></div>
+                <div class="quick" title="One-Click"><i class="fas fa-cloud-download-alt"></i></div>
+                <div class="beatsaver" title="BeatSaver">
                 <svg viewBox="0 0 177 177" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" clip-rule="evenodd" d="M3.51472 79.8822C-1.17157 84.5685 -1.17157 92.1665 3.51472 96.8528L79.8823 173.22C84.5685 177.907 92.1665 177.907 96.8528 173.22L173.22 96.8528C177.907 92.1665 177.907 84.5685 173.22 79.8823L96.8528 3.51472C92.1665 -1.17157 84.5685 -1.17157 79.8823 3.51472L3.51472 79.8822ZM144.529 83.309C146.086 84.8663 146.086 87.3912 144.529 88.9485L88.9486 144.529C87.3912 146.086 84.8664 146.086 83.3091 144.529C82.3293 143.549 81.9283 142.131 82.25 140.783L91.2463 103.094C92.6467 97.2274 97.2274 92.6467 103.094 91.2463L140.783 82.25C142.131 81.9283 143.549 82.3292 144.529 83.309Z" />
                 </svg>
                 </div>
-                <div class="download"><i class="fas fa-download"></i></div>
+                <div class="download" title="Download Files"><i class="fas fa-download"></i></div>
             </div>
         `;
         buttonParent.appendChild(buttonContainer);
